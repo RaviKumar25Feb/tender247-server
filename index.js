839f8ac5b675d4fb0c2c11ab19e4321ecd7d04b9
ravikumar25feb <RaviKumar25Feb@users.noreply.github.com>
2026-06-18 16:56:34 +0530

    added

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 // ================= CORS =================
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://tender247-website.vercel.app/", // 🔁 change this to your real frontend URL
+  "https://tender247-website.vercel.app",
+  "http://localhost:5153",
 ];
 
 app.use(
