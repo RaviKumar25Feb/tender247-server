eb4fc55cb340ae5a2bead0a01adba6883e871a76
ravikumar25feb <RaviKumar25Feb@users.noreply.github.com>
2026-06-22 09:58:10 +0530

    fixed cores

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // ================= CORS =================
 const allowedOrigins = [
   "https://tender247-website.vercel.app",
-  "http://localhost:5153",
+  "http://localhost:5173",
 ];
 
 app.use(
